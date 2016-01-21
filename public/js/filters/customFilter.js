5594a348291ed775a877e4e94e1413bf926e26b8
tykim89 <jusaha@naver.com>
2016-01-21 17:21:44 +0900

    create productshop : ajax, paging, category-css

--- public/js/filters/customFilter.js
+++ public/js/filters/customFilter.js
@@ -21,3 +21,28 @@
  		}
  	}
  });
+ .filter('range', function($filter){
+ 	return function(data, page, size){
+ 		if(angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)){
+ 			var startIndex = (page -1) * size;
+ 			if(data.length < startIndex){
+ 				return [];
+ 			}else{
+ 				return data;
+ 			}
+ 		}
+ 	}
+ });
+ .filter('pageCount', function(){	// 페이지 수
+ 	return function(data, size){
+ 		if(angular.isArray(data)){
+ 			var result = [];
+ 			for(var i=0; i<Math.ceil(data.length / size); i++){
+ 				result.push(i);
+ 			}
+ 			return result;
+ 		}else{
+ 			return data;
+ 		}
+ 	}
+ });
